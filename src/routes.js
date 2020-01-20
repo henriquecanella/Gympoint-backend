e894f6ac9c2bab3e1188615eb3f36724a58c76ba
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-20 19:43:28 -0300

    Cadastro de planos de treinamento

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -14,5 +15,10 @@ routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 routes.get('/students', StudentController.index);
 routes.delete('/students/:id', StudentController.delete);
+
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.get('/plans', PlanController.index);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
