337730726b8f341dd7212458fd5697a95c5e5e31
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-22 12:43:27 -0300

    Update de matriculas de usuários

--- src/app/controllers/EnrollmentController.js
+++ src/app/controllers/EnrollmentController.js
@@ -75,7 +75,64 @@ class EnrollmentController {
   }
 
   async update(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      plan_id: Yup.number(),
+      start_date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const enrollment = await Enrollment.findByPk(req.params.id);
+    const plan = await Plan.findByPk(enrollment.plan_id);
+
+    const { plan_id, start_date } = req.body;
+
+    let price;
+    let end_date;
+    const checkPlan = plan_id
+      ? await Plan.findOne({ where: { id: plan_id } })
+      : null;
+
+    if (plan_id && plan_id !== enrollment.plan_id) {
+      if (!checkPlan) {
+        return res.status(400).json({ error: 'This plan does not exists' });
+      }
+
+      price = checkPlan.price * checkPlan.duration;
+    } else {
+      price = enrollment.price;
+    }
+
+    if (start_date && start_date !== enrollment.start_date) {
+      if (isBefore(parseISO(start_date), new Date())) {
+        return res.status(400).json({ error: 'Past dates are not permitted' });
+      }
+      end_date = addMonths(
+        parseISO(start_date),
+        plan_id ? checkPlan.duration : plan.duration
+      );
+    } else {
+      end_date = addMonths(
+        enrollment.start_date,
+        plan_id ? checkPlan.duration : plan.duration
+      );
+    }
+
+    await enrollment.update({
+      price,
+      start_date,
+      end_date,
+      plan_id,
+    });
+
+    return res.json({
+      price,
+      start_date,
+      end_date,
+      plan_id,
+    });
   }
 
   async delete(req, res) {
